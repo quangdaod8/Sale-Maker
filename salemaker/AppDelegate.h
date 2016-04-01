be871bc5dc5da2617545a5adc58c95b3bb6e6265
Dao Duy Quang <daoduyquang91@gmail.com>
2016-04-01 09:21:39 +0700

    Initial Commit

---
+++ salemaker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  salemaker
+//
+//  Created by Duy Quang on 4/1/16.
+//  Copyright © 2016 duyquang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ salemaker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  salemaker
+//
+//  Created by Duy Quang on 4/1/16.
+//  Copyright © 2016 duyquang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
